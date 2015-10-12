c352e5b6ee67b48293605045d7c200a644a83527
rigor789 <rigor789@gmail.com>
2015-10-12 13:13:36 +0200

    Extracted commands to their own file. Extracted configuration to a separate file

--- bot.js
+++ bot.js
@@ -1,33 +1,34 @@
 require('dotenv').load();
 var irc = require('irc');
+var nconf = require('nconf');
+
 global.BOT = {};
 
-/**
- * Global bot settings
- */
-BOT.settings = {
+nconf.file({file: 'config.json'});
+
+nconf.defaults({
     name: 'rigitv', // the bots twitch username
     botname: 'rigibot', // the bots name (only for display in console)
     oauth: process.env.OAUTH, // the auth token for twitch
     channel: '#rigitv', // the channel this bot needs to join (ex. #rigitv)
     admins: ['rigitv'] // people who can use commands.
+});
+
+/**
+ * Global bot settings
+ */
+BOT.settings = {
+    name: nconf.get('name'),
+    botname: nconf.get('botname'),
+    oauth: nconf.get('oauth'),
+    channel: nconf.get('channel'),
+    admins: nconf.get('admins')
 };
 
 /**
  * The commands for this bot!
  */
-BOT.commands = {
-    'info': function () {
-        BOT.say("I'm a BOT by RigiTv! PogChamp");
-    },
-    'help': function (user) {
-        BOT.say("/me " + user + " here's no help for you son!");
-    },
-    'ping': function () {
-        BOT.say("/me PONG!!!");
-    },
-    'lines': require('./commands/lines.js')
-}
+BOT.commands = require('./commands.js')
 
 BOT.client = new irc.Client('irc.twitch.tv', BOT.settings.name, {
     sasl: true,
